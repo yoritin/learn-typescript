90c2f4a8963b033929ef87d56cfd1797160e3782
Makoto Katayori <47884591+yoritin@users.noreply.github.com>
2020-08-01 13:33:05 +0900

    clear

--- Makefile
+++
@@ -1,6 +0,0 @@
-build:
-	./node_modules/.bin/tsc
-
-run:
-	./node_modules/.bin/tsc
-	node ./dist/index.js
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-
-
-function squareOf(n: number) {
-  return n * n
-}
-
-const a = squareOf(8)
-console.log(a)
