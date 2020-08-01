a8548f8dc1a0e803cc94e1df71bf58f7283d9572
Makoto Katayori <47884591+yoritin@users.noreply.github.com>
2020-08-01 15:50:00 +0900

    class

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
-let a: {b: number} = {
-  b: 12
+let c: {
+  firstName: string
+  lastName: string
+} = {
+  firstName: 'john',
+  lastName: 'barrowman'
 }
-a.b
+
+class Person {
+  constructor(
+    public firstName: string,
+    public lastName: string
+  ) {}
+}
+c = new Person('matt', 'smith')
+console.log(c)
