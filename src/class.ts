c3be1aee1ffbbe23587240c60f7a13894dfd382c
Makoto Katayori <47884591+yoritin@users.noreply.github.com>
2020-08-24 08:04:56 +0900

    fix

--- src/class.ts
+++ src/class.ts
@@ -17,6 +17,7 @@ abstract class Person {
 }
 
 class Teacher extends Person {
+  private static instance: Teacher;
   explainJob() {
     console.log(`I am teacher and I teach ${this.subject}`)
   }
@@ -32,9 +33,15 @@ class Teacher extends Person {
     }
     this._subject = value
   }
-  constructor(name: string, age: number, private _subject: string) {
+  private constructor(name: string, age: number, private _subject: string) {
     super(name, age)
   }
+  static getInstance() {
+    if (Teacher.instance) return Teacher.instance;
+    Teacher.instance = new Teacher('Quill', 34, 'Math')
+    return Teacher.instance;
+  }
 }
-const teacher = new Teacher('Quill', 34, 'Math');
-teacher.greeting();
+const teacher = Teacher.getInstance();
+const teacher2 = Teacher.getInstance();
+console.log(teacher, teacher2)
