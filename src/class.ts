cae92386734d5e565eeced7d369df0705acee7d2
Makoto Katayori <47884591+yoritin@users.noreply.github.com>
2020-08-23 13:43:53 +0900

    add

--- src/class.ts
+++ src/class.ts
@@ -1,13 +1,19 @@
 class Person {
-  name: string;
-  constructor(initName: string) {
+  public name: string;
+  private age: number;
+  constructor(initName: string, initAge: number) {
     this.name = initName;
+    this.age = initAge;
   }
 
-  greeting() {
-    console.log(`Hello! My name is ${this.name}`);
+  incrementAge() {
+    this.age += 1;
+  }
+  greeting(this: Person) {
+    console.log(`Hello! My name is ${this.name}. I am ${this.age} years old.`);
   }
 }
 
-const quill = new Person('Quill');
+const quill = new Person('Quill', 25);
+quill.incrementAge();
 quill.greeting();
