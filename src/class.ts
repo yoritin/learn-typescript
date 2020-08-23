d9282629a91a51ca2f334d57cc97da354dff687b
Makoto Katayori <47884591+yoritin@users.noreply.github.com>
2020-08-23 13:17:04 +0900

    add class.ts

--- src/class.ts
+++ src/class.ts
@@ -3,7 +3,11 @@ class Person {
   constructor(initName: string) {
     this.name = initName;
   }
+
+  greeting() {
+    console.log(`Hello! My name is ${this.name}`);
+  }
 }
 
 const quill = new Person('Quill');
-console.log(quill);
+quill.greeting();
