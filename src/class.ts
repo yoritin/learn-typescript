e9272045d156e414539be7852986c3aca7cb6ffc
Makoto Katayori <47884591+yoritin@users.noreply.github.com>
2020-08-23 17:04:33 +0900

    fix

--- src/class.ts
+++ src/class.ts
@@ -1,11 +1,7 @@
 class Person {
-  public name: string;
-  private age: number;
-  constructor(initName: string, initAge: number) {
-    this.name = initName;
-    this.age = initAge;
-  }
 
+  constructor(public readonly name: string, protected age: number) {
+  }
   incrementAge() {
     this.age += 1;
   }
@@ -14,6 +10,27 @@ class Person {
   }
 }
 
-const quill = new Person('Quill', 25);
-quill.incrementAge();
-quill.greeting();
+class Teacher extends Person {
+  get subject() {
+    if (!this._subject) {
+      throw new Error('There is no subject.');
+    }
+    return this._subject;
+  }
+  set subject(value) {
+    if (!value) {
+      throw new Error('There is no subject.');
+    }
+    this._subject = value
+  }
+  constructor(name: string, age: number, private _subject: string) {
+    super(name, age)
+  }
+  greeting(this: Teacher) {
+    console.log(`Hello! My name is ${this.name}. I am ${this.age} years old. I teach ${this.subject}`);
+  }
+}
+const teacher = new Teacher('mail', 23, '');
+teacher.subject = 'Music'
+console.log(teacher.subject)
+teacher.greeting();
