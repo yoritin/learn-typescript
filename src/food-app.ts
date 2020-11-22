b16b4ff3a3730baeb061e0b88d59680121af9e72
yoritin <47884591+yoritin@users.noreply.github.com>
2020-11-22 16:06:38 +0900

    fact: get active food

--- src/food-app.ts
+++ src/food-app.ts
@@ -1,5 +1,18 @@
 class Score { }
-class Food { }
-class Foods {
-  elements = document.querySelectorAll('.food');
+class Food {
+  constructor(public element: HTMLDivElement) {
+    element.addEventListener('click', this.clickEventHandler.bind(this));
+  }
+  clickEventHandler() {
+    this.element.classList.toggle('food--active');
+  }
 }
+class Foods {
+  elements = document.querySelectorAll<HTMLDivElement>('.food');
+  constructor() {
+    this.elements.forEach(element => {
+      new Food(element);
+    })
+  }
+}
+const foods = new Foods();
