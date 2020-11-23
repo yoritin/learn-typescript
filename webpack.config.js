cf8c9c87074bbbdfb4ee53a567fcaef7993e1e5c
yoritin <47884591+yoritin@users.noreply.github.com>
2020-11-23 12:37:23 +0900

    fact: Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+console.log(path.resolve(__dirname, 'fafa', 'foo'));
+
+module.exports = {
+  entry: './dist/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
