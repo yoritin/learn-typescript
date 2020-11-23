81959d6310b653e1470cc316adab2bd36009862b
yoritin <47884591+yoritin@users.noreply.github.com>
2020-11-23 14:11:25 +0900

    fact: Add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
-console.log(path.resolve(__dirname, 'fafa', 'foo'));
 
 module.exports = {
   entry: './src/food-app/main.ts',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/'
   },
   devtool: 'inline-source-map',
   module: {
